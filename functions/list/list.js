a61ee9860c109ab20b1a357ec60e3a444363d11a
Kevin Lewis <kevin.lewis@vonage.com>
2020-08-03 17:05:38 +0100

    changed image qualities

--- functions/list/list.js
+++ functions/list/list.js
@@ -22,27 +22,13 @@ exports.handler = async (event, context) => {
       statusCode: 200,
       body: JSON.stringify(
         files.resources.map(file => {
-          if(file.resource_type == 'image') {
-            return {
-              url: cloudinary.url(file.public_id+'.jpg'),
-              thumbnail: cloudinary.image(file.public_id+'.jpg', {
-                height: 200
-              })
-            }
-          } else {
-            return {
-              url: cloudinary.url(file.public_id+'.mp4', { resource_type: 'video' }),
-              thumbnail: cloudinary.image(file.public_id+'.jpg', {
-                height: 200,
-                resource_type: 'video',
-                color: '#80d0f0',
-                overlay: {
-                  font_family: 'arial', 
-                  font_size: 40, 
-                  text: '▶'
-                }, 
-              })
-            }
+          return {
+            url: cloudinary.url(file.public_id+'.jpg', {
+              quality: 'auto'
+            }),
+            thumbnail: cloudinary.image(file.public_id+'.jpg', {
+              height: 400
+            })
           }
         })
       )
